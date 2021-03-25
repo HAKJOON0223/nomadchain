2ee0d62c957c291288da4f9bb29b4a43a7cf6c87
HAKJOON0223 <2hakjoon@gmail.com>
2021-03-25 22:14:50 +0900

    tsc watch added

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string):void => {
+    console.log(`hello ${name}, you are ${age }, you are a ${gender}`);
+}
+
+sayHi("nn", 24, "male");
+
+export {};
