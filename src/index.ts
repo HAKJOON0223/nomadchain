6e868998e8a334e39eb5f009c064942ec6c482ba
HAKJOON0223 <2hakjoon@gmail.com>
2021-03-25 22:39:24 +0900

    block chain first

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
-class Human{
-    public name : string;
-    public age : number;
-    public gender : string;
-    constructor(
-        name:string, age:number, gender:string
+class Block{
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data:string;
+    public timestamp:number;
+    constructor(index:number,
+        hash: string,
+        previousHash: string,
+        data:string,
+        timestamp:number
     ){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
-
-const lynn = new Human("hynn", 18, "femail");
-
-const person = {
-    name:"nicolas",
-    gender:"male",
-    age:22
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    };
 }
 
 
-const sayHi = (person:Human):void => {
-    console.log(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
-}
+const genesisBlock:Block = new Block(0, "222022220022", "", "Hello", 1234556);
 
-sayHi(lynn);
+let blockchain: [Block] = [genesisBlock];
 
+console.log(blockchain);
 export {};
