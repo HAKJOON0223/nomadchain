bba7b988e6d78baa6d8d6250185cf003ca4e3a1e
HAKJOON0223 <2hakjoon@gmail.com>
2021-03-28 10:30:22 +0900

    add new blockchain

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ class Block{
     static calculateBlockHash = (
         index:number, previouseHash:string, timestamp:number, data:string
         ):string => 
-        CryptoJS.SHA256(index + previouseHash + timestamp + data).toStirng();
+        CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
     
 
 
@@ -38,5 +38,17 @@ const getBlockchain = () : Block[] => blockchain;
 const getLatestBlock = () : Block => blockchain[blockchain.length -1];
 
 const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string):Block => {
+    const previosBlock : Block = getLatestBlock();
+    const newIndex : number = previosBlock.index + 1;
+    const newTimestamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
+    const newBlock : Block = new Block(newIndex, newHash, previosBlock.hash, data, newTimestamp)
+
+    return newBlock;
+}
+
+console.log(createNewBlock("hello"), createNewBlock("byebye"))
 
 export {};
